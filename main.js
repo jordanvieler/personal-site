01fad96b4318f1e53d4781f481bbffb956bdbb49
jordanvieler <jordan@vieler.org>
2022-01-12 17:23:54 -0800

    Created site, added responsive navbar

---
+++ main.js
@@ -0,0 +1,12 @@
+function flipOpen() {
+    var x = document.getElementById("responsiveNavbar");
+    if (x.className === "navbar") {
+      x.className += " open";
+    } else {
+      x.className = "navbar";
+    }
+  }
+
+function test_alert(){
+    alert("Nice!")
+}
